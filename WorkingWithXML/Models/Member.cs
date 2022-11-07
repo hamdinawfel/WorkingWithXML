cac27a1b9eae524f8731e6930e0569cc3c7121c3
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2022-11-07 22:50:18 +0100

    add xml serialization with attributes

--- WorkingWithXML/Models/Member.cs
+++ WorkingWithXML/Models/Member.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace WorkingWithXML.Models
 {
+    [Serializable]
+    [XmlRoot("MemberDetails")]
     public class Member
     {
+        [XmlElement("MemberName")]
         public string Name { get; set; }
+        [XmlElement("MemberEmailAddress")]
         public string Email { get; set; }
         public int Age { get; set; }
+        [XmlIgnore]
         public DateTime CreatedAt { get; set; }
+        [XmlAttribute("PremiumMember")]
         public bool IsPremium { get; set; } 
 
     }
